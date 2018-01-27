423eeac362b871f4b03c3382142478daaa196dac
MorningStar <792634614@qq.com>
2018-01-27 22:06:44 +0800

    再三考虑后决定采用原来的ui设计。。。。。

--- testProject/home.js
+++ testProject/home.js
@@ -7,67 +7,36 @@ import {
     TextInput,
     Keyboard,
     Dimensions,
+    Image,
+    ImageBackground,
 } from 'react-native';
 export default class HomePage extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            date: new Date(),
-            hour: new Date().getHours(),
-            min: new Date().getMinutes(),
-            sec: new Date().getSeconds(),
+            day: new Date().getDay(),
         }
         var timeUpdate = null;
     }
     componentDidMount() {
-        this.timeUpdate = setInterval(
-            () => {
-                if (this.state.sec < 10) {
-                    this.setState({
-                        sec: '0' + new Date().getSeconds()
-                    })
-                }
-                else{
-                    this.setState({
-                        sec: new Date().getSeconds()
-                    })
-                }
-                if (this.state.min < 10) {
-                    this.setState({
-                        min: '0' + new Date().getMinutes(),
-
-                    })
-                }
-                else {
-                    this.setState({
-                        min:  new Date().getMinutes()
-                    })
-                }
-                if (this.state.hour < 10) {
-                    this.setState({
-                        hour: '0' + new Date().getHours(),
-                    })
-                }
-                else{
-                    this.setState({
-                        hour:  new Date().getHours()
-                    })
-                }
-            }
-        )
     }
 
     render() {
         return (
             <View style={styles.topBox}>
-                <View style={styles.timeBox}>
-                    <Text style={styles.timeSpan}>{this.state.hour}</Text>
-                    <Text style={styles.timeDivide}>:</Text>
-                    <Text style={styles.timeSpan}>{this.state.min}</Text>
-                    <Text style={styles.timeDivide}>:</Text>
-                    <Text style={styles.timeSpan}>{this.state.sec}</Text>
-                </View>
+                <ImageBackground style={styles.topBoxBgI} source={require('./img/timg.jpg')}>
+                    <View style={styles.topLeftBox}>
+                        <View style={styles.topLeftWordBox}>
+                            <Text style={styles.topLeftWord}>week{this.state.day}</Text>
+                        </View>
+                    </View>
+                    <View style={styles.topRightBox}>
+
+                    </View>
+                </ImageBackground>
+
             </View>
+
         )
     }
 }
@@ -77,38 +46,76 @@ const windowPX = {
 }
 const styles = StyleSheet.create({
     topBox: {
-        height: windowPX.height * 0.40,
+        flex: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: windowPX.height * 0.30,
         width: windowPX.width,
         backgroundColor: '#00345a',
-        borderBottomLeftRadius: (windowPX.height * 0.40)/2,
-        borderBottomRightRadius: (windowPX.height * 0.40)/2,
-        // flex: 1,
-        // justifyContent: 'flex-end',
-        // alignItems: 'center',
-        shadowColor: '#ffffff',
-        shadowOffset: { width: 0, height: 0 },
-        shadowRadius: 10,
-        shadowOpacity: 1,
-        elevation:20,
+    },
+    topBoxBgI: {
+        height: windowPX.height * 0.30,
+        width: windowPX.width,
+    },
+    topLeftBox: {
+        flex: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: windowPX.height * 0.30,
+        width: windowPX.width * 0.36,
+        padding: windowPX.width * 0.02,
+    },
+    topLeftWordBox: {
+        flex: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: windowPX.height * 0.15,
+        width: windowPX.width * 0.18,
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        borderWidth: 1,
+        borderColor: '#fff',
+    },
+    topLeftWord: {
+        flex: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#fff',
+        fontSize: 28,
+        fontWeight: 'bold',
+        borderTopWidth: 6,
+        width: windowPX.width * 0.18,
+        height: windowPX.height * 0.12,
+        textAlign: 'center',
+        borderColor: '#fff',
+        paddingTop: windowPX.width * 0.02,
+    },
+    topRightBox: {
+        flex: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: windowPX.height * 0.30,
+        width: windowPX.width * 0.60,
+        padding: windowPX.width * 0.02,
     },
     timeBox: {
         flex: 1,
         flexDirection: 'row',
         height: windowPX.height * 0.26,
-        width: windowPX.width ,
+        width: windowPX.width,
         marginTop: windowPX.height * 0.14,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#ff6c00',
         borderTopLeftRadius: 15,
         borderTopRightRadius: 15,
-        borderBottomLeftRadius: (windowPX.height * 0.40)/2,
-        borderBottomRightRadius: (windowPX.height * 0.40)/2,
+        borderBottomLeftRadius: (windowPX.height * 0.40) / 2,
+        borderBottomRightRadius: (windowPX.height * 0.40) / 2,
         shadowColor: '#ffffff',
         shadowOffset: { width: 0, height: 0 },
         shadowRadius: 10,
         shadowOpacity: 1,
-        elevation:4,
+        elevation: 4,
     },
     timeSpan: {
         height: windowPX.height * 0.06,
